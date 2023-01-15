81f9a3f985cb4574a3ec407a1f49597ba8734757
Kota Asai <kota@KotanoMacBook-Air.local>
2023-01-15 20:49:30 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>main branch: {{ msg }}</div>
+  <!-- <Tutorial /> -->
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data() {
+    return {
+      msg: process.env.msg
+    }
+  }
+})
+</script>
